189e8587180f55ddbfcc71931fb32be791b9b4ae
Almeida123007 <kamabokugonpachiro11@gmail.com>
2025-08-14 13:09:32 -0300

    Add files via upload

---
+++ TEMPERATURA.py
@@ -0,0 +1,9 @@
+temperatura = float(input("Digite a temperatura: "))
+if temperatura < 10:
+    print("Frio🥶🥶🥶")
+elif temperatura < 20:
+    print("Temperatura normal🙂🙂🙂")
+elif temperatura < 30:
+    print("Calor😯😯😯")
+else:
+    print("Muito calor🥵🥵🥵")
